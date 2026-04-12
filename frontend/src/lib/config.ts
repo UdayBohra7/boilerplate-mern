f14d5ec325c11822fdb3d05d98aaea0e3f0de5d1
UdayBohra7 <143582686+UdayBohra7@users.noreply.github.com>
2026-04-12 18:25:43 +0530

    config changed

--- frontend/src/lib/config.ts
+++ frontend/src/lib/config.ts
@@ -1,3 +1,2 @@
 export const API_URL = import.meta.env.VITE_API_URL as string;
 export const BASE_URL = import.meta.env.VITE_BASE_URL as string || API_URL.replace('/v1', '');
-export const JWT_SECRET = '123456' as string;
